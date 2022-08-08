7e0bbda58ef36bdbdf0acf16de92bd89aecaf402
Shreeya Nelekar <40059938+Shreeya1699@users.noreply.github.com>
2022-08-08 23:57:38 +0530

    Time: 10 ms (97.42%), Space: 10.3 MB (85.38%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int>timestamp(10001,0);
+        int maxDrop=0;
+        for(int i=0;i<trips.size();i++){
+            if(trips[i][2]>maxDrop)
+                maxDrop=trips[i][2];
+        }
+        vector<int>timestamp(maxDrop+1,0);
         for(int i=0;i<trips.size();i++){
             timestamp[trips[i][1]] +=trips[i][0];
             timestamp[trips[i][2]] -=trips[i][0];
         }
-        for(int i=0;i<=1000;i++){
+        for(int i=0;i<=maxDrop;i++){
             capacity -=timestamp[i];
             if(capacity<0)
                 return false;
         }
         return true;
+        
     }
 };
